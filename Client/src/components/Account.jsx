dd8ab9d4ec6842a968b030d67823f0faa689a753
nitin272 <nitinson95092@gmail.com>
2024-04-02 16:02:03 +0530

    login full functionality

--- Client/src/components/Account.jsx
+++ Client/src/components/Account.jsx
@@ -1,56 +1,106 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Account.css';
 import Navbar from "../components/Navbar";
-import { FaEdit } from 'react-icons/fa'; 
+import { FaEdit } from 'react-icons/fa';
+import Loading from './Load';
+import './Account.css';
+
 const Account = () => {
     const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [isEditing, setIsEditing] = useState({ name: false, email: false });
+    const [editData, setEditData] = useState({ name: '', email: '' });
     const navigate = useNavigate();
 
     useEffect(() => {
-        const storedUserData = JSON.parse(localStorage.getItem('user') || '{}');
-      
-        const { userId, token, name, email, imageUrl } = storedUserData.user || storedUserData;
+        const loadingTimeout = setTimeout(() => {
+            const storedUserData = JSON.parse(localStorage.getItem('user') || '{}');
+            const { userId, token, name, email, imageUrl } = storedUserData.user || storedUserData;
 
-        if (!userId) {
-            console.log('Redirecting to login page.');
-            navigate('/login');
-            return;
-        }
-        
-      
-        if ((name || storedUserData.username) && email) {
-            setUserData({
-                ...storedUserData.user, 
-                ...storedUserData, 
-            });
-            return;
-        }
-
-        const fetchUserData = async () => {
-            try {
-                const response = await fetch(`https://explorexpress-4.onrender.com/auth/user/${userId}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    },
-                });
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch user data: ${response.statusText}`);
-                }
-                const data = await response.json();
-                setUserData(data);
-            } catch (error) {
-                console.error('Error:', error);
+            if (!userId) {
+                console.log('Redirecting to login page.');
+                navigate('/login');
+                return;
             }
-        };
 
-        fetchUserData();
+            if ((name || storedUserData.username) && email) {
+                setUserData({
+                    ...storedUserData.user,
+                    ...storedUserData,
+                });
+                setLoading(false);
+                return;
+            }
+
+            const fetchUserData = async () => {
+                try {
+                    const response = await fetch(`https://explorexpress-4.onrender.com/auth/user/${userId}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`,
+                        },
+                    });
+                    if (!response.ok) {
+                        throw new Error(`Failed to fetch user data: ${response.statusText}`);
+                    }
+                    const data = await response.json();
+                    setUserData(data);
+                } catch (error) {
+                    console.error('Error:', error);
+                } finally {
+                    setLoading(false);
+                }
+            };
+
+            fetchUserData();
+        }, 500);
+
+        return () => clearTimeout(loadingTimeout);
     }, [navigate]);
 
-   
-    const userName = userData?.name || userData?.username;
+    const handleEdit = (field) => {
+        setIsEditing({ ...isEditing, [field]: true });
+        setEditData({ ...editData, [field]: userData[field] });
+    };
+
+    const handleChange = (e) => {
+        setEditData({ ...editData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (field) => {
+        setLoading(true);
+        try {
+            const response = await fetch(`https://localhost:4000/auth/user/update/${userData.userId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${userData.token}`,
+                },
+                body: JSON.stringify({ [field]: editData[field] }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Failed to update user data: ${response.statusText}`);
+            }
+            const updatedData = await response.json();
+            setUserData({ ...userData, ...updatedData });
+            setIsEditing({ ...isEditing, [field]: false });
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) {
+        return (
+            <>
+                <Navbar />
+                <Loading />
+            </>
+        );
+    }
 
     return (
         <>
@@ -59,16 +109,38 @@ const Account = () => {
                 {userData ? (
                     <>
                         <div className="user-detail">
-                            <h1>Welcome, {userName}!</h1>
-                            <FaEdit className="edit-icon" />
+                            <h1>Welcome, {isEditing.name ? (
+                                <>
+                                    <input type="text" name="name" value={editData.name} onChange={handleChange} />
+                                    <button onClick={() => handleSubmit('name')}>Save</button>
+                                </>
+                            ) : (
+                                <>
+                                    {userData.name || userData.username}
+                                    <FaEdit className="edit-icon" onClick={() => handleEdit('name')} />
+                                </>
+                            )}!</h1>
                         </div>
                         <div className="user-detail">
-                            <p>Email: {userData.email}</p>
-                            <FaEdit className="edit-icon" />
+                            <p>Email: {isEditing.email ? (
+                                <>
+                                    <input type="email" name="email" value={editData.email} onChange={handleChange} />
+                                    <button onClick={() => handleSubmit('email')}>Save</button>
+                                </>
+                            ) : (
+                                <>
+                                    {userData.email}
+                                    <FaEdit className="edit-icon" onClick={() => handleEdit('email')} />
+                                </>
+                            )}</p>
                         </div>
-                        {userData.imageUrl && <img src={userData.imageUrl} alt="User Profile" />}
+                        {userData.imageUrl && (
+                            <img src={userData.imageUrl} alt="User Profile" />
+                        )}
                     </>
-                ) : <div>Please log in to view your account.</div>}
+                ) : (
+                    <div>Please log in to view your account.</div>
+                )}
             </div>
         </>
     );
