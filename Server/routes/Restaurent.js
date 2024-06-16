c52c3298dceb42ac58d086755427d80ab548d295
nitin272 <nitinson95092@gmail.com>
2024-06-16 15:04:52 +0530

    put api used

--- Server/routes/Restaurent.js
+++ Server/routes/Restaurent.js
@@ -8,6 +8,7 @@ router.get('/restaurants/:id', RestaurantController.getRestaurantById);
 router.post('/restaurants', RestaurantController.createRestaurant);
 
 router.put('/restaurants/:id', RestaurantController.updateRestaurant);
+
 router.delete('/restaurants/:id', RestaurantController.deleteRestaurant);
 
 module.exports = router;
